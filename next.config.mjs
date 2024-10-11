0c5e81bc0d8b898fc254d43ec765feba66dda1ac
Megh <79590183+hikki78@users.noreply.github.com>
2024-10-11 17:48:46 +0530

    idk man

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   eslint: {
-      ignoreDuringBuilds: true,
-    },
-    images: { unoptimized: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
