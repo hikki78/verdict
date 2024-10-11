2a026eb754b36835ee185aee8f2578c250374579
Megh <79590183+hikki78@users.noreply.github.com>
2024-10-11 17:54:19 +0530

    appDir sucks

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,18 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  experimental: {
+    appDir: true,
+  },
+   webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
